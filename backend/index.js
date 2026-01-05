b8aeeb71d8ff249c5fb46dcdf86830bf3074640f
unknown <nbpntp@gmail.com>
2026-01-04 20:52:36 -0500

    Added /countries endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,28 @@
-require("dotenv").config()
-const express = require("express")
-const { unknownEndpointHandler, errorHandler } = require("./middleware/errorHandlers")
 const morgan = require("./middleware/morgan")
+const express = require("express")
+const axios = require("axios")
+const cors = require("cors")
+const { 
+  unknownEndpointHandler, 
+  errorHandler 
+} = require("./middleware/errorHandlers")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 // _______________________________________
 
 
+app.get("/countries", async (req, res) => {
+  const data = await axios
+    .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+    .then(res => res.data)
+
+  res.json(data)
+})
 
 
 // _______________________________________
